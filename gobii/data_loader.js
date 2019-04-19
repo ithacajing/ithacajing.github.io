5633a67a59dc9683c743b17e7ce6a32817c28997
Jane Zhang <jing.zhang9@gmail.com>
2019-04-19 16:32:51 -0400

    new content

--- gobii/data_loader.js
+++ gobii/data_loader.js
@@ -1,8 +1,40 @@
-//alert("hello");
-//checked and unchecked the radio button             
-//if clicked the historical data, set the date sent by vendor to N/A
+
 $(document).ready(function(){
-    
+    //check the form
+    /*function validate() {
+        if(document.getElementByName("crop").checked = true){
+           alert( "Please choose a certain crop!" );
+           document.myForm.crop.focus() ;
+           return false;
+        }
+        if( document.myForm.othercrop.value == "" ) {
+           alert( "Please enter the other crop name!" );
+           document.myForm.othercrop.focus() ;
+           return false;
+        }
+        /*if( document.myForm.EMail.value == "" ) {
+           alert( "Please provide your Email!" );
+           document.myForm.EMail.focus() ;
+           return false;
+        }
+        if( document.myForm.Zip.value == "" || isNaN( document.myForm.Zip.value ) ||
+           document.myForm.Zip.value.length != 5 ) {
+           
+           alert( "Please provide a zip in the format #####." );
+           document.myForm.Zip.focus() ;
+           return false;
+        }
+        if( document.myForm.Country.value == "-1" ) {
+           alert( "Please provide your country!" );
+           return false;
+        }
+        return( true );
+     }
+     */
+
+
+    //checked and unchecked the radio button             
+    //if clicked the historical data, set the date sent by vendor to N/A
     $('#otherC').hide();
     $('#otherI').hide();
 
